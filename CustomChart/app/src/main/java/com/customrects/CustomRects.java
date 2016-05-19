ba0dc7b48055428b55551335c9583ef8500e7052
supermanmwg <554607289@qq.com>
2016-05-19 18:16:33 +0800

    地下城市与勇士，哈哈

--- CustomChart/app/src/main/java/com/customrects/CustomRects.java
+++ CustomChart/app/src/main/java/com/customrects/CustomRects.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -12,9 +14,14 @@ import android.view.View;
  * Created by mwg on 16-5-18.
  */
 public class CustomRects extends View {
+    private static final String TAG = "CustomRects";
+
+    private float defaultTextSize = 15;
     private Paint mPaint;
     private int widthInterval = 50;
-    private int destHeight = 500;
+    private int destHeight = 400;
+    private float mScale = 1;
+    private int rectCount = 10;
 
     public CustomRects(Context context) {
         this(context, null);
@@ -32,6 +39,7 @@ public class CustomRects extends View {
 
     private void init() {
         mPaint = new Paint();
+        mPaint.setTextSize(defaultTextSize * mScale);
         mPaint.setAntiAlias(true);
         //mPaint.setStyle(Paint.Style.STROKE);
     }
@@ -40,22 +48,87 @@ public class CustomRects extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.translate((getWidth() - widthInterval * 30) / 2,600);
-        for (int i = 0; i < 30; i++) {
-            int srcHeight = (int) (Math.random() * destHeight);
-            Rect rect = new Rect(i * widthInterval + 5, srcHeight, (i+1) * widthInterval, destHeight);
+      //  Log.d(TAG, "height is " + getHeight());
+        if (destHeight > getHeight() - 10) {
+            destHeight = getHeight() - 10;
+        }
+
+        canvas.translate((getWidth() - widthInterval * rectCount * mScale) / 2,(getHeight() - destHeight * mScale) /2);
+        for (int i = 0; i < rectCount; i++) {
+            float srcHeight = (float) (Math.random() * destHeight * mScale);
+            RectF rect = new RectF(i * widthInterval * mScale + 5 * mScale, srcHeight, (i + 1) * widthInterval * mScale, destHeight * mScale);
             canvas.drawRect(rect, mPaint);
 
-            String text = "" + (500  - srcHeight);
+          String text = "" + (destHeight - (int) srcHeight);
+            mPaint.setTextSize(defaultTextSize * mScale);
             float textLength = mPaint.measureText(text, 0, text.length());
-           // Paint.FontMetrics textFontMetric = ;
-            float begin =(widthInterval - 5 - textLength) / 2 + i * widthInterval + 5;
-            canvas.drawText(text, begin, srcHeight - 10, mPaint);
+            Paint.FontMetrics textFontMetric = mPaint.getFontMetrics();
+            float begin = ((widthInterval - 5) * mScale - textLength) / 2 + i * widthInterval * mScale + 5 * mScale;
+            float baseLine = srcHeight  - textFontMetric.bottom;
+            canvas.drawText(text, begin, baseLine, mPaint);
         }
     }
 
+    private int mLastX;
+    private int mLastY;
+    private int mFirstPointerId;
+
+    private int mOldDest;
+    private int mCurrentDest;
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction() & MotionEvent.ACTION_MASK;
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d(TAG, "down index: " + event.getActionIndex() + ", event pointer id: "+ event.getPointerId(event.getActionIndex()));
+                mLastX = (int) event.getX();
+                mLastY = (int) event.getY();
+                mFirstPointerId = event.getPointerId(event.getActionIndex());
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                Log.d(TAG, "down point index: " + event.getActionIndex() + ", event pointer id: "+ event.getPointerId(event.getActionIndex()));
+                if(event.getPointerCount() > 1) {
+                    int x1 = (int) event.getX(0);
+                    int y1 = (int) event.getY(0);
+                    int x2 = (int) event.getX(1);
+                    int y2 = (int) event.getY(1);
+
+                    mOldDest =Math.max(1, (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if(event.getPointerCount() == 1) {
+                    int x = (int) event.getX();
+                    int y = (int) event.getY();
+                    if(event.getPointerId(event.getActionIndex()) == mFirstPointerId) {
+                        int offsetX = mLastX - x;
+                        int offsetY = mLastY - y;
+                        scrollBy(offsetX, offsetY);
+                    }
+                    mLastX = x;
+                    mLastY = y;
+                } else if (event.getPointerCount() > 1) {
+                    int x1 = (int) event.getX(0);
+                    int y1 = (int) event.getY(0);
+                    int x2 = (int) event.getX(1);
+                    int y2 = (int) event.getY(1);
+                    mCurrentDest= Math.max(1, (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 -y1)));
+                    mScale = (float)mCurrentDest / (float)mOldDest * mScale;
+                //    Log.d(TAG, "scale is " + mScale + ", mCurrent dest is " + mCurrentDest + ", mOld dest is " + mOldDest);
+                   mOldDest = mCurrentDest;
+                    invalidate();
+                }
+
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.d(TAG, "up  index: " + event.getActionIndex() + ", event pointer id: "+ event.getPointerId(event.getActionIndex()));
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                Log.d(TAG, "up point index: " + event.getActionIndex() + ", event pointer id: "+ event.getPointerId(event.getActionIndex()));
+                break;
+        }
+
         return true;
     }
 }
--- CustomChart/app/src/main/java/com/customrects/MainActivity.java
+++ CustomChart/app/src/main/java/com/customrects/MainActivity.java
@@ -1,5 +1,6 @@
 package com.customrects;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
         setContentView(R.layout.activity_main);
     }
 }
